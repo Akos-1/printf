fa7ab61f173aa380dc35ebb22dccce1eee0ffa1e
Chris-Effah <akboateng41@gmail.com>
2023-05-27 14:23:37 -0700

    update

--- chrisvee_printf.c
+++ chrisvee_printf.c
@@ -1,45 +1,53 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "main.h"
+
 /**
- * _printf -  a function that displays output according to a format.
- * @format:  a character string.
- * Return: the number of characters printed
+ * _printf - a function that displays output according to a format.
+ * @format: a character string.
+ * Return: the number of characters printed.
  */
 int _printf(const char *format, ...)
 {
 	int chars_displayed = 0;
-	char arg, new_char, *st;
+	char present, arg, new_char, *st;
 	va_list argu;
 
 	va_start(argu, format);
-	while (*format != '\0')
+	while ((present = *format++) != '\0')
 	{
-		new_char = *format;
-		switch (new_char)
+		if (new_char != '\0')
 		{
-			case 'c':
+			_putchar(present);
+			chars_displayed++;
+		}
+		else
+		{
+			new_char = *format++;
+
+			if (new_char == 'c')
+			{
 				arg = (char)va_arg(argu, int);
 				_putchar(arg);
 				chars_displayed++;
-				break;
-			case 's':
+			}
+			else if (new_char == 's')
+			{
 				st = va_arg(argu, char*);
 				while (*st != '\0')
 				{
+					_putchar(*st++);
 					chars_displayed++;
 				}
-				break;
-			case '%':
+			}
+			else if (new_char == '%')
+			{
 				_putchar('%');
 				chars_displayed++;
-				break;
-			default:
-				break;
+			}
 		}
-		format++;
 	}
-
 	va_end(argu);
 	return (chars_displayed);
 }
+
