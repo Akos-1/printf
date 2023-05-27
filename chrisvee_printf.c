5479af507dbdec49b51ce6a6db6f47985dec7ed9
Chris-Effah <akboateng41@gmail.com>
2023-05-27 09:49:15 -0700

    created our printf function

---
+++ chrisvee_printf.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+  * _printf -  a function that displays output according to a format.
+  * @format:  a character string.
+  * Return: the number of characters printed
+  */
+int _printf(const char *format, ...)
+{
+	int chars_displayed = 0;
+	char present, arg, new_char, *st;
+	va_list argu;
+
+	va_start(argu, format);
+
+	while ((chars_displayed = *format++) != '\0')
+	{
+		if (new_char != '\0')
+			our_putchar(present);
+			chars_displayed++;
+		else
+		{
+			new_char = *format++;
+			switch (new_char)
+			{
+				case 'c':
+					arg = (char)va_arg(argu, int);
+					our_putchar(arg);
+					chars_displayed++;
+					break;
+				case 's':
+					st = va_arg(argu, char*);
+					while (*st != '\0')
+					{
+						our_putchar(*st++);
+						chars_displayed++;
+					}
+					break;
+				case '%':
+					our_putchar('%');
+					chars_displayed++;
+					break;
+				default:
+					break;
+			}
+		}
+	}
+	va_end(argu);
+	return (chars_displayed);
+}
