16a726f0b4e6b805c80754ea9f3eecf39abf7c4b
Akos-1 <veronicagyasi@gmail.com>
2023-05-29 14:05:09 -0700

    Update

---
+++ chrisvee_printf.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
+
+/**
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
+int _printf(const char *format, ...)
+{
+	int a, displayed = 0, chars_displayed = 0;
+	int flg, wid, pr, size, buff_ind = 0;
+	va_list argu;
+	char buffer[BUFFER_SIZE];
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(argu, format);
+
+	for (a = 0; format && format[a] != '\0'; i++)
+	{
+		if (format[a] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFFER_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[a], 1);*/
+			chars_displayed++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flg = get_flg(format, &a);
+			wid = get_wid(format, &a, argu);
+			pr = get_pr(format, &a, argu);
+			size = get_size(format, &a);
+			++a;
+			displayed = handle_print(format, &a, argu, buffer,
+					flg, wid, pr, size);
+			if (displayed == -1)
+				return (-1);
+			chars_displayed += displayed;
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(argu);
+
+	return (chars_displayed);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
---
+++ main.h
@@ -0,0 +1,125 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define UNUSED(x) (void)(x)
+#define BUFFER_SIZE 1024
+
+/* FLAGS */
+#define F_MINUS (1 << 0)
+#define F_PLUS (1 << 1)
+#define F_ZERO (1 << 2)
+#define F_HASH (1 << 3)
+#define F_SPACE (1 << 4)
+
+/* SIZES */
+#define S_LONG (1 << 0)
+#define S_SHORT (1 << 1)
+
+/**
+ * struct frt - Struct op
+ * @frt: The format.
+ * @fun: The function associated with the format.
+ */
+struct frt
+{
+	char frt;
+	int (*fun)(va_list, char[], int, int, int, int);
+}
+
+/**
+ * typedef struct frt frt_t - Struct op
+ * @frt: The format.
+ * @fr_t: The function associated.
+ */
+typedef struct frt frt_t;
+
+int _printf(const char *format, ...);
+int handle_print(const char *frt, int *i,
+		va_list argu, char buffer[], int flg, int wid, int pr, int size);
+
+/****************** FUNCTIONS ******************/
+
+/* Funtions to print chars and strings */
+int print_char(va_list typ, char buffer[],
+		int flg, int wid int pr, int size);
+int print_str(va_list typ, char buffer[],
+		int flg, int wid int pr, int size);
+
+int print_per(va_list typ, char buffer[],
+		int flg, int wid int pr, int size);
+
+
+/* Functions to print numbers */
+int print_int(va_list typ, char buffer[],
+		int flg, int wid int pr, int size);
+
+int print_binary(va_list typ, char buffer[],
+		int flg, int wid int pr, int size);
+
+int print_unsigned(va_list typ, char buffer[],
+		int flg, int wid int pr, int size);
+
+int print_oct(va_list typ, char buffer[],
+		int flg, int wid int pr, int size);
+
+int print_hexadec(va_list typ, char buffer[],
+		int flg, int wid int pr, int size);
+
+int print_hexa_upp(va_list typ, char buffer[],
+		int flg, int wid int pr, int size);
+
+
+int print_hexa(va_list typ, char map_to[],
+		char buffer[], int flg, char flg_ch, int wid, int pr, int size);
+
+/* Function to print non printable characters */
+int print_non_printable(va_list typ, char buffer[],
+		int flg, int wid, int pr, int size);
+
+/* Function to print memory address */
+int print_point(va_list typ, char buffer[],
+		int flg, int wid int pr, int size);
+
+/* Functions to handle other specifiers */
+int get_flg(const char *format, int *a);
+int get_wid(const char *format, int *a, va_list argu);
+int get_pr(const char *format, int *a, va_list argu);
+int get_size(const char *format, int *a);
+
+/*Function to print string in reverse*/
+int print_rev(va_list typ, char buffer[],
+		int flg, int wid, int pr, int size);
+
+/*Function to print a string in rot 13*/
+int print_rot13str(va_list typ, char buffer[],
+		int flg, int wid int pr, int size);
+
+/* width handler */
+int handle_write_char(char c, char buffer[],
+		int flg, int wid int pr, int size);
+
+int write_number(int is_positive, int ind, char buffer[],
+		int flg, int wid int pr, int size);
+
+int write_num(int ind, char bff[], int flg, int wid, int pr,
+		int len, char padd, char extra_char);
+int write_pointer(char buffer[], int ind, int len,
+		int wid, int flg, char padd, char extra_char, int padd_start);
+
+int write_unsgnd(int is_negative, int ind,
+		char buffer[],
+		int flg, int wid int pr, int size);
+
+
+/****************** UTILS ******************/
+int is_print(char);
+int append_hexa_code(char, char[], int);
+int is_dig(char);
+
+long int conv_size_num(long int num, int size);
+long int conv_size_unsgnd(unsigned long int num, int size);
+
+#endif /* MAIN_H */
